beb6d1c1630763eaa75269abcd63b414179e350d
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2023-09-30 17:32:38 -0400

    Improved floating-point methods.
    * 128-bit method now uses the 64-bit floating-point method combined with the
      original method for the rest of the bits.
    * Added optimization assumptions regarding the range of outputs.

--- src/floating_point.rs
+++ src/floating_point.rs
@@ -10,7 +10,18 @@ pub trait UnsignedIsqrt {
 
 impl SignedIsqrt for i8 {
     fn checked_isqrt(self) -> Option<Self> {
-        (self >= 0).then(|| (self as f32).sqrt() as Self)
+        (self >= 0).then(|| {
+            let result = (self as f32).sqrt() as Self;
+
+            // SAFETY: the result is nonnegative and less than or equal to `i64::MAX.isqrt()`.
+            // Inform the optimizer about it.
+            unsafe {
+                intrinsics::assume(0 <= result);
+                intrinsics::assume(result <= 11);
+            }
+
+            result
+        })
     }
 
     #[inline]
@@ -22,13 +33,32 @@ impl SignedIsqrt for i8 {
 
 impl UnsignedIsqrt for u8 {
     fn isqrt(self) -> Self {
-        (self as f32).sqrt() as Self
+        let result = (self as f32).sqrt() as Self;
+
+        // SAFETY: the result fits in an integer with half as many bits.
+        // Inform the optimizer about it.
+        unsafe {
+            intrinsics::assume(result < 1 << 4);
+        }
+
+        result
     }
 }
 
 impl SignedIsqrt for i16 {
     fn checked_isqrt(self) -> Option<Self> {
-        (self >= 0).then(|| (self as f32).sqrt() as Self)
+        (self >= 0).then(|| {
+            let result = (self as f32).sqrt() as Self;
+
+            // SAFETY: the result is nonnegative and less than or equal to `i16::MAX.isqrt()`.
+            // Inform the optimizer about it.
+            unsafe {
+                intrinsics::assume(0 <= result);
+                intrinsics::assume(result <= 181);
+            }
+
+            result
+        })
     }
 
     #[inline]
@@ -40,13 +70,32 @@ impl SignedIsqrt for i16 {
 
 impl UnsignedIsqrt for u16 {
     fn isqrt(self) -> Self {
-        (self as f32).sqrt() as Self
+        let result = (self as f32).sqrt() as Self;
+
+        // SAFETY: the result fits in an integer with half as many bits.
+        // Inform the optimizer about it.
+        unsafe {
+            intrinsics::assume(result < 1 << 8);
+        }
+
+        result
     }
 }
 
 impl SignedIsqrt for i32 {
     fn checked_isqrt(self) -> Option<Self> {
-        (self >= 0).then(|| (self as f64).sqrt() as Self)
+        (self >= 0).then(|| {
+            let result = (self as f64).sqrt() as Self;
+
+            // SAFETY: the result is nonnegative and less than or equal to `i32::MAX.isqrt()`.
+            // Inform the optimizer about it.
+            unsafe {
+                intrinsics::assume(0 <= result);
+                intrinsics::assume(result <= 46_340);
+            }
+
+            result
+        })
     }
 
     #[inline]
@@ -58,7 +107,15 @@ impl SignedIsqrt for i32 {
 
 impl UnsignedIsqrt for u32 {
     fn isqrt(self) -> Self {
-        (self as f64).sqrt() as Self
+        let result = (self as f64).sqrt() as Self;
+
+        // SAFETY: the result fits in an integer with half as many bits.
+        // Inform the optimizer about it.
+        unsafe {
+            intrinsics::assume(result < 1 << 16);
+        }
+
+        result
     }
 }
 
@@ -71,6 +128,12 @@ impl SignedIsqrt for i64 {
             let mut result = (self as u64 as f64).sqrt() as Self;
             if result * result > self {
                 result -= 1;
+            }
+            // SAFETY: the result is nonnegative and less than or equal to `i64::MAX.isqrt()`.
+            // Inform the optimizer about it.
+            unsafe {
+                intrinsics::assume(0 <= result);
+                intrinsics::assume(result <= 3_037_000_499);
             }
 
             result
@@ -97,6 +160,12 @@ impl UnsignedIsqrt for u64 {
             result -= 1;
         }
 
+        // SAFETY: the result fits in an integer with half as many bits.
+        // Inform the optimizer about it.
+        unsafe {
+            intrinsics::assume(result < 1 << 32);
+        }
+
         result
     }
 }
@@ -107,7 +176,16 @@ impl SignedIsqrt for i128 {
         if self < 0 {
             None
         } else {
-            Some((self as u128).isqrt() as Self)
+            let result = (self as u128).isqrt() as Self;
+
+            // SAFETY: the result is nonnegative and less than or equal to `i128::MAX.isqrt()`.
+            // Inform the optimizer about it.
+            unsafe {
+                intrinsics::assume(0 <= result);
+                intrinsics::assume(result <= 13_043_817_825_332_782_212);
+            }
+
+            Some(result)
         }
     }
 
@@ -119,27 +197,42 @@ impl SignedIsqrt for i128 {
 }
 
 impl UnsignedIsqrt for u128 {
-    // Uses technique from
-    // https://web.archive.org/web/20220118185505/https://www.codecodex.com/wiki/Calculate_an_integer_square_root#Java
-    fn isqrt(self) -> Self {
+    fn isqrt(mut self) -> Self {
+        // The algorithm is based on the one presented in
+        // <https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Binary_numeral_system_(base_2)>
+        // which cites as source the following C code:
+        // <https://web.archive.org/web/20120306040058/http://medialab.freaknet.org/martin/src/sqrt/sqrt.c>.
+
         let leading_zeros = self.leading_zeros();
         let result = if leading_zeros >= 64 {
             (self as u64).isqrt() as Self
         } else {
-            let mut bit_index = (65 - leading_zeros) & 0b1111110;
-            let mut result = ((self >> bit_index) as u64).isqrt() as Self;
-            bit_index >>= 1;
-            result <<= bit_index;
-            let mut bit = 1 << bit_index;
-            while bit > 0 {
-                bit >>= 1;
-                let result_high = result | bit;
-                if result_high * result_high <= self {
-                    result = result_high;
+            let even_bit_index = (65 - leading_zeros) & 0b1111110;
+
+            let mut result = ((self >> even_bit_index) as u64).isqrt() as Self;
+            self -= (result * result) << even_bit_index;
+            result <<= even_bit_index;
+            let mut even_bit = 1 << (even_bit_index - 2);
+
+            while even_bit != 0 {
+                let result_high = result | even_bit;
+                if self >= result_high {
+                    self -= result_high;
+                    result = (result >> 1) | even_bit;
+                } else {
+                    result >>= 1;
                 }
+                even_bit >>= 2;
             }
+
             result
         };
+
+        // SAFETY: the result fits in an integer with half as many bits.
+        // Inform the optimizer about it.
+        unsafe {
+            intrinsics::assume(result < 1 << 64);
+        }
 
         result
     }
